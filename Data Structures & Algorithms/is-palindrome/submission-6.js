e63b990efca499c6fb69c2ada7d00a0e9a1bef24
jonguan1 <jonguan@gmail.com>
2026-06-18 21:18:45 -0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        var front = 0;
+        var back = s.length - 1;
+        while (front < back) {
+            var frontChar = s.charAt(front);
+            if(!this.isAlphanumeric(frontChar)){
+                front++;
+                continue;
+            } else {
+                frontChar = frontChar.toLowerCase();
+            }
+            var backChar = s.charAt(back);
+            if(!this.isAlphanumeric(backChar)){
+                back--;
+                continue;
+            } else {
+                backChar = backChar.toLowerCase();
+
+            }
+            if (frontChar !== backChar){
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+
+    isAlphanumeric(input) {
+        const pattern = /^[a-z0-9]+$/i; // i flag for case-insensitive
+        return pattern.test(input);
+    }
+
+}
