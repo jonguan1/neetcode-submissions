766cbcc03a4850219601882271c4cffc69e66bb7
jonguan1 <jonguan@gmail.com>
2026-07-17 06:42:14 -0700

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    int[]cache;
+    public int minCostClimbingStairs(int[] cost) {
+        cache = new int[cost.length];
+        Arrays.fill(cache, -1);
+        return Math.min(minCost(cost, 0), minCost(cost,1));
+    }
+    public int minCost(int[]cost, int index){
+        if (index == cost.length-1 || index == cost.length-2){
+            return cost[index];
+        }
+        if (cache[index] != -1){
+            return cache[index];
+        }
+        cache[index] = cost[index] + Math.min(minCost(cost, index+1), minCost(cost, index+2));
+        return cache[index];
+    }
+}
